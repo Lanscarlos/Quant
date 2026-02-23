6dcb8a54dbb464c45ed3a3391409d9af3d1f2088
Lanscarlos <lanscarlos@hotmail.com>
2026-02-23 17:46:47 +0900

    feat: 完成 NiceGUI 完整打包的技术选型验证

--- build.py
+++ build.py
@@ -1,35 +1,30 @@
 # build.py
+# 构建 EXE 可执行程序
 
-import PyInstaller.__main__
+import os, subprocess
+import shutil
+from pathlib import Path
+import nicegui, webview
 
-# 定义你的主脚本文件名
-SOURCE_FILE = 'main.py'
+print("开始打包，请稍候...\n")
 
-# 定义生成的 exe 名字（可选，不写默认和脚本同名）
-EXE_NAME = 'Quant'
+# 运行 PyInstaller
 
-# 定义图标路径（如果有的话，没有就留空或注释掉）
-ICON_PATH = 'assets/icon.ico'
+if os.path.exists('build'):
+    shutil.rmtree('build')
+    print("已清理 build 临时文件夹")
 
-# 组装打包参数列表
-params = [
-    SOURCE_FILE,  # 你的主程序文件
-    '-F',  # 打包成一个单独的 exe 文件
-    '-w',  # 这里的 -w 意思是不要黑框 (Windowed)
-    '--clean',  # 每次打包前清理缓存
-    f'-n={EXE_NAME}',  # 命名你的 exe 文件
+if os.path.exists(f'{EXE_NAME}.spec'):
+    os.remove(f'{EXE_NAME}.spec')
+    print("已清理 spec 配置文件")
 
-    # 如果你有图标，取消下面这行的注释
-    f'-i={ICON_PATH}',
+print(f"打包完成！请在 dist 文件夹中查看 {EXE_NAME}.exe")
+
+cmd = [
+'PyInstaller', 'app.py',
+'--name', 'MyApp',
+'--onefile', '--windowed', '--clean',
+'--add-data', f'{Path(nicegui.__file__).parent}{os.pathsep}nicegui',
+'--add-data', f'{Path(webview.__file__).parent}{os.pathsep}webview'
 ]
-
-def build():
-    print("开始打包，请稍候...\n")
-
-    # 运行 PyInstaller
-    PyInstaller.__main__.run(params)
-
-    print(f"打包完成！请在 dist 文件夹中查看 {EXE_NAME}.exe")
-
-if __name__ == "__main__":
-    build()
+subprocess.call(cmd)
