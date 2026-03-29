921a980ce2222b3fb6da756bd0369fbf14a0e027
Lanscarlos <lanscarlos@hotmail.com>
2026-03-29 09:20:30 +0900

    feat: 临时增加 TODO List

--- src/ui/panel/info.py
+++ src/ui/panel/info.py
@@ -1,6 +1,9 @@
 from nicegui import ui
 
 def render():
-    ui.label('📋 测试').classes('text-2xl font-bold')
+    ui.label('📋 TODO 待办事项').classes('text-2xl font-bold')
     ui.separator()
-    ui.label('这里是测试内容区域')
+    ui.checkbox(value=False, text="增加历史数据页面")
+    ui.checkbox(value=False, text="赛事详情页增加顶部功能按钮区域")
+    ui.checkbox(value=False, text="赛事详情页增加亚盘数据渲染逻辑")
+    ui.checkbox(value=False, text="优化抓取数据效率")
