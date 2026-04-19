6d4181b24b00e295e41a8e239543b84259b5b0a6
Lanscarlos <lanscarlos@hotmail.com>
2026-04-19 13:08:50 +0900

    refactor: 移除归档文件

--- archived/src/ui/panel/info.py
+++
@@ -1,9 +0,0 @@
-from nicegui import ui
-
-def render():
-    ui.label('📋 TODO 待办事项').classes('text-2xl font-bold')
-    ui.separator()
-    ui.checkbox(value=False, text="增加历史数据页面")
-    ui.checkbox(value=False, text="赛事详情页增加顶部功能按钮区域")
-    ui.checkbox(value=False, text="赛事详情页增加亚盘数据渲染逻辑")
-    ui.checkbox(value=False, text="优化抓取数据效率")
